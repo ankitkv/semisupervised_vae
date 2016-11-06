6846b86f28c118c11480a88aa8f969a4333bc594
Ankit Vani <a@nevitus.org>
2016-11-06 01:10:01 -0400

    fix code formatting

--- data/mnist.py
+++ data/mnist.py
@@ -1,13 +1,12 @@
-###
 '''
 Borrowed from original implementation: https://github.com/dpkingma/nips14-ssl (anglepy)
 '''
-###
 
 import numpy as np
 import cPickle, gzip
 import data
 import os
+
 
 def load_numpy(path, binarize_y=False):
     # MNIST dataset
@@ -22,8 +21,9 @@ def load_numpy(path, binarize_y=False):
         train_y = binarize_labels(train_y)
         valid_y = binarize_labels(valid_y)
         test_y = binarize_labels(test_y)
-        
+
     return train_x.T, train_y, valid_x.T, valid_y, test_x.T, test_y
+
 
 # Loads data where data is split into class labels
 def load_numpy_split(path, binarize_y=False, n_train=50000):
@@ -32,7 +32,7 @@ def load_numpy_split(path, binarize_y=False, n_train=50000):
 
     train_x = train_x[0:n_train]
     train_y = train_y[0:n_train]
-    
+
     def split_by_class(x, y, num_classes):
         result_x = [0]*num_classes
         result_y = [0]*num_classes
@@ -41,7 +41,7 @@ def load_numpy_split(path, binarize_y=False, n_train=50000):
             result_x[i] = x[:,idx_i]
             result_y[i] = y[idx_i]
         return result_x, result_y
-    
+
     train_x, train_y = split_by_class(train_x, train_y, 10)
     if binarize_y:
         valid_y = binarize_labels(valid_y)
@@ -58,9 +58,11 @@ def binarize_labels(y, n_classes=10):
         new_y[y[i], i] = 1
     return new_y
 
+
 def unbinarize_labels(y):
     return np.argmax(y,axis=0)
-    
+
+
 def save_reshaped(shape):
     def reshape_digits(x, shape):
         def rebin(a, shape):
@@ -83,7 +85,8 @@ def save_reshaped(shape):
     f = gzip.open(os.path.dirname(__file__)+'/mnist_'+str(shape[0])+'_.pkl.gz','wb')
     cPickle.dump((train, valid, test), f)
     f.close()
-    
+
+
 def make_random_projection(shape):
     W = np.random.uniform(low=-1, high=1, size=shape)
     W /= (np.sum(W**2,axis=1)**(1./2)).reshape((shape[0],1))
@@ -98,7 +101,8 @@ def create_semisupervised(x, y, n_labeled):
     import random
     n_x = x[0].shape[0]
     n_classes = y[0].shape[0]
-    if n_labeled%n_classes != 0: raise("n_labeled (wished number of labeled samples) not divisible by n_classes (number of classes)")
+    if n_labeled%n_classes != 0: raise("n_labeled (wished number of labeled samples) not "
+                                       "divisible by n_classes (number of classes)")
     n_labels_per_class = n_labeled/n_classes
     x_labeled = [0]*n_classes
     x_unlabeled = [0]*n_classes
@@ -111,6 +115,5 @@ def create_semisupervised(x, y, n_labeled):
         y_labeled[i] = y[i][:,idx[:n_labels_per_class]]
         x_unlabeled[i] = x[i][:,idx[n_labels_per_class:]]
         y_unlabeled[i] = y[i][:,idx[n_labels_per_class:]]
-    return np.hstack(x_labeled), np.hstack(y_labeled), np.hstack(x_unlabeled), np.hstack(y_unlabeled)
-        
-        
+    return np.hstack(x_labeled), np.hstack(y_labeled), np.hstack(x_unlabeled), \
+           np.hstack(y_unlabeled)
